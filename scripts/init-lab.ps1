3d836bea327d7ff44318ff3b6408c9ee04b9a1a3
ESSARRAJ Fouad <fouad.systems@gmail.com>
2024-01-11 12:26:24 +0100

    init labels

---
+++ scripts/init-lab.ps1
@@ -0,0 +1,39 @@
+# Script de création d'un lab-web
+
+## issue : édition de script avec des caractètre en français
+# à  éditer avec Encoding : Windows 1252
+
+# issue : Problème d'affichage des caractère en français sur la console
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+
+
+# Paramètres 
+# $labr_reference=$args[0]
+param ($labr_reference='lab-web-1')
+
+# Confirmation
+$title    = "Création de lab $labr_reference "
+$question = "Are you sure you want to proceed?"
+$choices  = '&Yes', '&No'
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 1) {
+    Write-Host 'cancelled'
+    exit
+} 
+
+
+Write-Host "Initialisation de lab : $labr_reference "
+
+# Création de fichier .code-workspace de vs code
+$work_space_file_name = "$labr_reference.code-workspace"
+new-item $work_space_file_name
+Set-Content $work_space_file_name '{"folders": [{"path": "."}],"settings": {}}'
+
+# init gitflow 
+git flow init
+
+#  set develop branch as default 
+git push --set-upstream origin develop
+gh repo edit --default-branch develop
+
+# Add Branch protection rules
---
+++ scripts/init-labels.ps1
@@ -0,0 +1,1 @@
+gh label delete documentation
