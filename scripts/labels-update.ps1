03a3fcde7208ce40f863c8ddf6339c2e2d573332
ESSARRAJ Fouad <fouad.systems@gmail.com>
2024-01-14 18:51:16 +0100

    rename scripts

---
+++ scripts/labels-update.ps1
@@ -0,0 +1,10 @@
+gh label delete documentation --yes
+gh label delete duplicate --yes
+gh label delete 'good first issue' --yes
+gh label delete 'help wanted' --yes
+gh label delete invalid --yes
+gh label delete question --yes
+gh label delete wontfix --yes
+gh label delete enhancement --yes
+
+gh label clone https://github.com/labs-web/gestion-projet.git
